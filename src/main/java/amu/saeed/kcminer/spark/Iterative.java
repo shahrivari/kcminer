612de13f6b3681a648d635466bac759d7a6f8ee4
Saeed Shahrivari <saeed.shahrivari@gmail.com>
2015-09-27 22:06:43 +0330

    Cogrouped again.

--- src/main/java/amu/saeed/kcminer/spark/Iterative.java
+++ src/main/java/amu/saeed/kcminer/spark/Iterative.java
@@ -2,6 +2,7 @@ package amu.saeed.kcminer.spark;
 
 import amu.saeed.kcminer.graph.KCliqueState;
 import com.google.common.base.Stopwatch;
+import com.google.common.collect.Iterables;
 import org.apache.spark.Accumulator;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
@@ -14,10 +15,7 @@ import org.kohsuke.args4j.Option;
 import scala.Tuple2;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Iterative {
     public static void main(String[] args) throws IOException {
@@ -75,6 +73,7 @@ public class Iterative {
         biggerNeighbors = biggerNeighbors.repartition(params.graphParts).persist(StorageLevel.MEMORY_AND_DISK());
 
 
+
         JavaRDD<KCliqueState> states =
             biggerNeighbors.map(t -> new KCliqueState(t._1, t._2)).repartition(params.numTasks);
 
@@ -85,17 +84,34 @@ public class Iterative {
                 for (int i = 0; i < x.extSize; i++)
                     list.add(new Tuple2<>(x.extension[i], x));
                 return list;
-            }).join(biggerNeighbors, params.numTasks).flatMap(t -> {
+            }).cogroup(biggerNeighbors, params.numTasks).flatMap(t -> {
                 int w = t._1;
-                int[] w_neighs = t._2._2;
-                List<KCliqueState> list = new ArrayList<>();
-                list.add(t._2._1.expand(w, w_neighs));
-                counts[iteration].add((long) list.size());
-                return list;
+                Iterator<int[]> w_iter = t._2._2.iterator();
+                int[] w_neighs = w_iter.next();
+                if (w_iter.hasNext())
+                    throw new IllegalStateException("This must not happen!");
+                return Iterables.transform(t._2._1, s -> {
+                    counts[iteration].add(1L);
+                    return s.expand(w, w_neighs);
+                });
             });
+
+            //            join(biggerNeighbors, params.numTasks).flatMap(t -> {
+            //                int w = t._1;
+            //                int[] w_neighs = t._2._2;
+            //                List<KCliqueState> list = new ArrayList<>();
+            //                list.add(t._2._1.expand(w, w_neighs));
+            //                counts[iteration].add((long) list.size());
+            //                return list;
+            //            });
         }
 
-        counts[params.k].setValue(states.map(t -> (long) t.extSize).reduce((a, b) -> a + b));
+        final int k = params.k;
+        states.map(t -> {
+            counts[k].add((long) t.extSize);
+            return t.extSize;
+        }).count();
+
 
         for (int i = 1; i < counts.length; i++)
             System.out.printf("Cliques of size %d => %,d\n", i, counts[i].value());
@@ -103,7 +119,6 @@ public class Iterative {
         sc.close();
 
         System.out.println("Took: " + stopwatch);
-
     }
 
 
