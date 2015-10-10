5cac80f49280e1f739a0cca314524c5d1cd05514
Saeed Shahrivari <saeed.shahrivari@gmail.com>
2015-10-10 22:54:18 +0330

    Added PECO!

--- src/main/java/amu/saeed/kcminer/smp/Main.java
+++ src/main/java/amu/saeed/kcminer/smp/Main.java
@@ -1,69 +1,49 @@
 package amu.saeed.kcminer.smp;
 
 import amu.saeed.kcminer.graph.Graph;
-import amu.saeed.kcminer.old.OldGraph;
 import com.google.common.base.Stopwatch;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Saeed on 8/15/2015.
  */
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        int size = 4;
-        int threads = 8;
-        String graph_path = "X:\\networks\\kcminer\\wikivote.txt";
-        OldGraph graph;
-        Graph ngraph;
-        long count;
-        Stopwatch stopwatch = Stopwatch.createUnstarted();
-
-        //        stopwatch.reset().start();
-        //        graph = OldGraph.buildFromEdgeListFile(graph_path);
-        ngraph = Graph.buildFromEdgeListFile(graph_path);
-        //        System.out.println(graph.getInfo());
-        //        System.out.println("Took:" + stopwatch);
-        //        stopwatch.reset().start();
-        //        count = CliqueEnumerator.parallelEnumerate(new RawCliqueStateManager(), graph, size, threads, null);
-        //        System.out.printf("%,d\n", count);
-        //        System.out.println("Took:" + stopwatch);
-        //        System.out.println("=========================");
-        //
-        //
-        //        stopwatch.reset().start();
-        //        graph = PrunedGraph.buildFromEdgeListFile(graph_path);
-        //        System.out.println(graph.getInfo());
-        //        System.out.println("Took:" + stopwatch);
-        //        stopwatch.reset().start();
-        //        count = CliqueEnumerator.parallelEnumerate(new PrunedCliqueStateManager(), graph, size, threads,
-        // null);
-        //        System.out.printf("%,d\n", count);
-        //        System.out.println("Took:" + stopwatch);
-        //        System.out.println("=========================");
-        //
-        //        stopwatch.reset().start();
-        //        count = CliqueEnumerator.parallelCount(new PrunedCliqueStateManager(), graph, size, threads);
-        //        System.out.printf("%,d\n", count);
-        //        System.out.println("Took:" + stopwatch);
-
-        for (int k = 1; k <= size; k++) {
-            stopwatch.reset().start();
-            System.out.println("*************************************************");
-            count = KCliqueEnumerator.parallelTurboCount(ngraph, k, threads);
-            System.out.printf("%d -> %,d\n", k, count);
-            System.out.println("Took:" + stopwatch);
+        Params params = new Params();
+        try {
+            new CmdLineParser(params).parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            new CmdLineParser(params).printUsage(System.err);
+            return;
         }
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        Graph graph = Graph.buildFromEdgeListFile(params.inputPath);
+        System.out.printf("Loaded the graph in %s.\n", stopwatch.toString());
+        stopwatch.reset().start();
 
-        //        stopwatch.reset().start();
-        //        System.out.println("*************************************************");
-        //        count = KCliqueEnumerator.parallelEnumerate(ngraph, size, threads, null);
-        //        System.out.printf("%,d\n", count);
-        //        System.out.println("Took:" + stopwatch);
+        long count;
+        if (params.turboCount)
+            count = KCliqueEnumerator.parallelTurboCount(graph, params.k, params.numThreads);
+        else
+            count = KCliqueEnumerator.parallelEnumerate(graph, params.k, params.numThreads, null);
+        System.out.printf("Graph: %s   size: %d   threads: %d   count: %,d\t time: %,d\n",
+            new File(params.inputPath).getName(), params.k, params.numThreads, count,
+            stopwatch.elapsed(TimeUnit.MILLISECONDS));
+    }
 
 
-
-
+    private static class Params {
+        @Option(name = "-k", usage = "size of the cliques to mine.", required = true) int k;
+        @Option(name = "-t", usage = "number of threads to launch", required = true) int numThreads;
+        @Option(name = "-i", usage = "the input path", required = true) String inputPath;
+        @Option(name = "-c", usage = "Turbo count") boolean turboCount = false;
     }
 
 }
